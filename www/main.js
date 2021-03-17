880cd732b9ba254a7916c11720b945d07530549d
lingan233 <71675451+lingan233@users.noreply.github.com>
2021-03-17 19:10:47 -0400

    Add files via upload

--- www/main.js
+++ www/main.js
@@ -1,33 +1,17 @@
-const cat1obj = document.getElementById('cat1');
-const cat2obj = document.getElementById('cat2');
-const cat3obj = document.getElementById('cat3');
-const cat4obj = document.getElementById('cat4');
-const cat5obj = document.getElementById('cat5');
+function myFunction() {
+    var x = document.getElementById("my-links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
 
-cat1obj.addEventListener('click', function(){
-    console.log('Cat1 clicked on')
-    UserPicked('cat1');
-});
-
-cat2obj.addEventListener('click', function(){
-    console.log('Cat2 clicked on')
-});
-
-cat3obj.addEventListener('click', function(){
-    console.log('Cat3 clicked on')
-});
-
-cat4obj.addEventListener('click', function(){
-    console.log('Cat4 clicked on')
-});
-
-cat5obj.addEventListener('click', function(){
-    console.log('Cat5 clicked on')
-});
-
-function UserPicked(whichOne){
-    console.log('User Picked Called ' + whichOne);
-    //Display large img
-    //Display Date Range
-    //Play item sound
+function myFunction1() {
+    var x = document.getElementById("my-works");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
 }
